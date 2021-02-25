0a5c2775be6fba1cf09020b0544c7ccf9691384c
prodigysurge <boatinglife6@yahoo.com>
2021-02-25 10:15:54 -0500

    commit tree a8e27544dec021af36b46bacd70f5d5690b53124

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |p|
+      p.string :username
+      p.string :password_digest
+    end
   end
   
   def down
+    drop_table :users
   end
 end
